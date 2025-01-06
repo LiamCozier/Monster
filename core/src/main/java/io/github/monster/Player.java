c7767d32de0d59cf18d213eaa7f9d3f248e4438a
LiamCozier <19cozier.l@isaacnewtonsixthform.co.uk>
2025-01-06 15:11:22 +0000

    movement grid update

--- core/src/main/java/io/github/monster/Player.java
+++ core/src/main/java/io/github/monster/Player.java
@@ -1,6 +1,8 @@
 package io.github.monster;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 
 public class Player extends Entity {
@@ -9,8 +11,19 @@ public class Player extends Entity {
         super(x_pos, y_pos, Color.PINK);
     }
 
-    public void take_input(int[] grid_dimensions) {
+    public void take_input(int grid_size) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+            x_pos-=1;
+        }if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+            x_pos+=1;
+        }if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            y_pos+=1;
+        }if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            y_pos-=1;
+        }
 
+        x_pos = Math.clamp(x_pos, 0, grid_size-1);
+        y_pos = Math.clamp(y_pos, 0, grid_size-1);
     }
 
 }
--- core/src/main/java/io/github/monster/World.java
+++ core/src/main/java/io/github/monster/World.java
@@ -2,31 +2,29 @@ package io.github.monster;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import org.w3c.dom.css.RGBColor;
 
 public class World {
     Player player;
     int[][] grid;
-    int[] grid_dimensions = {16, 16};
-    int square_spacing = 7;
-    int square_size = (640/grid_dimensions[1])-square_spacing;
+    int grid_size = 16;
+    int square_spacing = (128/grid_size);
+    int square_size = (640/ grid_size)-square_spacing;
 
 
     World() {
-        grid = new int[grid_dimensions[0]][grid_dimensions[1]];
         player = new Player(4, 4);
     }
 
     public void update() {
-        player.take_input(grid_dimensions);
+        player.take_input(grid_size);
     }
 
     public void draw(ShapeRenderer sr) {
 
         int square_offset = square_size + square_spacing;
 
-        for (int i=0; i<grid_dimensions[0]; i++) {
-            for (int j=0; j<grid_dimensions[1]; j++) {
+        for (int i = 0; i< grid_size; i++) {
+            for (int j = 0; j< grid_size; j++) {
                 Color background_color = new Color(0.2f, 0.2f, 0.27f, 1f);
                 sr.setColor(background_color);
                 sr.rect(i * square_offset + ((float) square_spacing/2), j * square_offset + ((float) square_spacing/2), square_size, square_size);
